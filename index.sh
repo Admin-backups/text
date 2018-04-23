c5183da57ee78cee9c39a609c77452199c5638a2
Admin-backups <wwb601237044@hotmail.com>
2018-04-24 01:05:49 +0800

    Create index.sh

---
+++ index.sh
@@ -0,0 +1,25 @@
+#Install
+cd /root
+wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+
+curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
+source ~/.bash_profile
+nvm install v9.8.0
+nvm alias default v9.8.0
+
+yum -y install gcc-c++
+yum -y install flex
+yum -y install bison
+wget https://raw.githubusercontent.com/Admin-backups/text/libpcap-1.8.1.tar.gz
+tar -zxvf libpcap-1.8.1.tar.gz
+cd libpcap-1.8.1
+./configure
+make
+make install
+
+cd libpcap-1.8.1/scichicken/
+npm i
+npm i -g pino
+ifconfig
