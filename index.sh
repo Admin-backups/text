187fb7e569c4400f452f34b09a27dfb19b03412f
Admin-backups <wwb601237044@hotmail.com>
2018-04-25 12:35:24 +0800

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,8 +1,8 @@
 #Start Install
 cd /root
-wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
-chmod +x shadowsocks-all.sh
-./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh
+chmod +x shadowsocks.sh
+./shadowsocks.sh 2>&1 | tee shadowsocks.log
 
 yum -y install gcc-c++
 yum -y install flex
@@ -24,12 +24,13 @@ nvm install v9.8.0
 nvm alias default v9.8.0
 
 cd /root/libpcap-1.8.1/
-wget https://raw.githubusercontent.com/Admin-backups/text/master/kkshen.tar.gz
-tar -zxvf kkshen.tar.gz
-cd /root/libpcap-1.8.1/kkshen/
+wget https://raw.githubusercontent.com/Admin-backups/text/master/archive.tar.gz
+tar -zxvf archive.tar.gz
+cd /root/libpcap-1.8.1/winnerpubg/
 npm i
 npm i -g pino
-ifconfig
+npm install -g forever
 
+echo "搭建完成"
 echo '谨慎使用！仅供研究！'
-echo 'node index.js sniff eth0 1**.***.***.*** | pino'
+echo 'forever start index.js sniff eth0 $ip | pino'
